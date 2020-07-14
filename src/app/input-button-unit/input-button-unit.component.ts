0d430c8c760e9f1e852816ee0202c9d8f08e6a95
Buga Victoria <victoriabuga00@gmail.com>
2020-07-14 15:27:38 +0300

    add todo with local storage

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,18 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  templateUrl: './input-button-unit.component.html',
+  styleUrls: ['./input-button-unit.component.css']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World!';
+  @Output() submit: EventEmitter<string> = new EventEmitter();
+  ngOnInit() {
+  }
+
+submitValue(newTitle: string) {
+    this.submit.emit(newTitle);
+}
+
+}
---
+++ src/app/services/todo-list.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+import { StorageService } from './storage.service';
+
+const todoListStorageKey = 'Todo_List';
+const defaultTodoList: TodoItem[] = [
+  {title: 'Make exercices'},
+  {title: 'Learn Angular'},
+  {title: 'More practice'}
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoListService {
+  todoList: TodoItem[];
+
+  constructor(private storageService: StorageService) {
+    this.todoList = storageService.getData(todoListStorageKey) || defaultTodoList;
+  }
+
+  saveList() {
+    this.storageService.setData(todoListStorageKey, this.todoList);
+  }
+
+  addItem(item: TodoItem) {
+    this.todoList.push(item);
+    this.saveList();
+  }
+
+  updateItem(item: TodoItem, changes) {
+    const index = this.todoList.indexOf(item);
+    this.todoList[index] = { ...item, ...changes };
+    this.saveList();
+  }
+
+  deleteItem(item: TodoItem) {
+    const index = this.todoList.indexOf(item);
+    this.todoList.splice(index, 1);
+    this.saveList();
+  }
+
+  editItem(item: TodoItem) {
+    const index = this.todoList.indexOf(item);
+
+  }
+
+  getTodoList() {
+    return this.todoList;
+  }
+
+}
